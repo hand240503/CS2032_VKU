fe256ad2d5d73eb5bd8235615d85f4f26db0bd98
hand240503 <nguyendangha2405@gmail.com>
2025-02-27 13:40:56 +0700

    lab 5

---
+++ xylophone/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Xylophone(),
+      ),
+    ),
+  );
+}
+
+class Xylophone extends StatelessWidget {
+  final AudioPlayer player = AudioPlayer(); // Tạo một instance của AudioPlayer
+
+  void playSound(int noteNumber) async {
+    await player.play(AssetSource('audio/note$noteNumber.wav'));
+  }
+
+  Expanded buildKey({required Color color, required int noteNumber}) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          playSound(noteNumber);
+        },
+        style: TextButton.styleFrom(
+            backgroundColor: color,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(0))),
+        child: const SizedBox.shrink(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          buildKey(color: Colors.red, noteNumber: 1),
+          buildKey(color: Colors.orange, noteNumber: 2),
+          buildKey(color: Colors.yellow, noteNumber: 3),
+          buildKey(color: Colors.green, noteNumber: 4),
+          buildKey(color: Colors.teal, noteNumber: 5),
+          buildKey(color: Colors.blue, noteNumber: 6),
+          buildKey(color: Colors.purple, noteNumber: 7),
+        ],
+      ),
+    );
+  }
+}
